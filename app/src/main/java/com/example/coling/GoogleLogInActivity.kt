b1e8c5d911494f61c8994af2eee3926f7b87ea05
pabaep <sewon225@gmail.com>
2021-02-01 22:42:03 +0900

    시도

---
+++ app/src/main/java/com/example/coling/GoogleLogInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleLogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_log_in)
+    }
+}

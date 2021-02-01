a3715ad16e63e5576ab7ce935c0f48b7d150760f
unknown <jeehea99@naver.com>
2021-02-01 16:04:49 +0900

    my act, history 레이아웃

---
+++ app/src/main/java/com/example/coling/MyActActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyActActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_act)
+    }
+}

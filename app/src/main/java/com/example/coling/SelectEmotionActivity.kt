61c90c6cc85b048fa46738c3c088b29dfdcb5b8e
pabaep <sewon225@gmail.com>
2021-02-02 19:37:19 +0900

    sewon ui 수정

--- app/src/main/java/com/example/coling/SelectEmotionActivity.kt
+++ app/src/main/java/com/example/coling/SelectEmotionActivity.kt
@@ -3,9 +3,9 @@ package com.example.coling
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ActRecodeActivity : AppCompatActivity() {
+class SelectEmotionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_act_recode)
+        setContentView(R.layout.activity_select_emotion)
     }
 }

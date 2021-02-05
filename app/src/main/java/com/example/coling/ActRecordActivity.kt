d92ac911faadd372bc92e71eca5b4ff41d27d364
pabaep <sewon225@gmail.com>
2021-02-05 23:41:12 +0900

    sewon기초화면 연결

--- app/src/main/java/com/example/coling/ActRecordActivity.kt
+++ app/src/main/java/com/example/coling/ActRecordActivity.kt
@@ -10,9 +10,24 @@ class ActRecordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_act_record)
 
-        back_btn.setOnClickListener({
+        back_btn.setOnClickListener {
             val intent = Intent(this, RandomActActivity::class.java)
             startActivity(intent)
-        })
+        }
+
+        act_record_emotion_img.setOnClickListener {
+            val intent = Intent(this, SelectEmotionActivity::class.java)
+            startActivity(intent)
+        }
+
+        act_record_photo_img.setOnClickListener {
+            //카메라 사진찍기 구현
+        }
+
+        act_record_save_btn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)//기록 보기 화면으로 추후 전환
+            startActivity(intent)
+        }
     }
+
 }

f19a6f4956aa3930bfabf01a8cdc8fa9bc57c4b4
unknown <jeehea99@naver.com>
2021-03-03 17:25:05 +0900

    putExtra

--- app/src/main/java/com/example/coling/MyActActivity.kt
+++ app/src/main/java/com/example/coling/MyActActivity.kt
@@ -3,7 +3,10 @@ package com.example.coling
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_my_act.*
 import kotlinx.android.synthetic.main.activity_random_act.*
+import kotlinx.android.synthetic.main.activity_random_act.back
+import kotlinx.android.synthetic.main.activity_random_act.btn_doit
 
 class MyActActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +16,9 @@ class MyActActivity : AppCompatActivity() {
 
         btn_doit.setOnClickListener{
             val intent = Intent(this, ActRecordActivity::class.java)
+            intent.putExtra("act_name", my_act_name.text.toString())
+            intent.putExtra("act_content", my_act_contents.text.toString())
             startActivity(intent)
-            finish()
         }
 
         back.setOnClickListener{
--- app/src/main/java/com/example/coling/RandomActActivity.kt
+++ app/src/main/java/com/example/coling/RandomActActivity.kt
@@ -3,26 +3,18 @@ package com.example.coling
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_random_act.*
 
 class RandomActActivity : AppCompatActivity() {
-
-//    private lateinit var actName :TextView
-//    private lateinit var actContent :TextView
-    //private val actName :TextView = random_act_name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random_act)
 
-//        actName = findViewById<TextView>(R.id.random_act_name)
-//        actContent = findViewById<TextView>(R.id.random_act_content)
-
         btn_doit.setOnClickListener{
             val intent = Intent(this, ActRecordActivity::class.java)
-           //intent.putExtra("act_name", actName.text.toString())
+            intent.putExtra("act_name", random_act_name.text.toString())
+            intent.putExtra("act_content", random_act_content.text.toString())
             startActivity(intent)
         }
 

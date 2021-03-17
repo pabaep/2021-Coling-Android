e05540f7e0bf69d683d7102082038937657ac02a
pabaep <sewon225@gmail.com>
2021-03-17 22:02:18 +0900

    toast 영어로 수정, 기록하기 기능, 기록하기 날짜띄우기

--- app/src/main/java/com/example/coling/model/ModelRecords.kt
+++ app/src/main/java/com/example/coling/model/ModelRecords.kt
@@ -3,12 +3,13 @@ package com.example.coling.model
 data class ModelRecords(var uid : String? = null,
                         var day : Long? = null,
                         var date : Long? = null,
-                        var act_id : Int? = null,
                         var act_name : String? = null,
                         var act_content : String? = null,
                         var img_src : String? = null,
                         var emo : String? = null,
-                        var diary : String? = null,
-                        var day_check : Boolean? = null){
+                        var diary : String? = null
+                        //var act_id : Int? = null,
+                        //var day_check : Boolean? = null
+                        ){
 
 }

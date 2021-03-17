4ac9641d7e5e82180bce79880880149e4f31eee3
pabaep <sewon225@gmail.com>
2021-03-17 19:28:40 +0900

    day 아마 수정, 날짜 불러오는거 성공

--- app/src/main/java/com/example/coling/model/ModelUser.kt
+++ app/src/main/java/com/example/coling/model/ModelUser.kt
@@ -1,4 +1,5 @@
 package com.example.coling.model
 
 data class ModelUser(var uid : String? = null,
-                     var start_date : Long? = null) {}
+                     var start_date : Long? = null,
+                     var act_num : Int? = 1) {}

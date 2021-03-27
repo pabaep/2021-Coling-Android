9eb5ddaaf3e467a76d98d0b3d5da8c088ca7930e
pabaep <sewon225@gmail.com>
2021-03-27 22:38:20 +0900

    my act 막기

--- app/src/main/java/com/example/coling/MyActActivity.kt
+++ app/src/main/java/com/example/coling/MyActActivity.kt
@@ -3,6 +3,7 @@ package com.example.coling
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_my_act.*
 import kotlinx.android.synthetic.main.activity_random_act.*
 import kotlinx.android.synthetic.main.activity_random_act.back
@@ -12,14 +13,24 @@ class MyActActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_act)
-
+        var myActName = my_act_name.text.toString()
+        var myActContent = my_act_contents.text.toString()
 
         btn_doit.setOnClickListener{
             val intent = Intent(this, ActRecordActivity::class.java)
-            intent.putExtra("act_name", my_act_name.text.toString())
+            /*intent.putExtra("act_name", my_act_name.text.toString())
             intent.putExtra("act_content", my_act_contents.text.toString())
             intent.putExtra("act_num", 0)
-            startActivity(intent)
+            startActivity(intent)*/
+            if(myActName==""||myActContent==""){
+                Toast.makeText(this,"Write act name or act content",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                intent.putExtra("act_name",myActName)
+                intent.putExtra("act_content",myActContent)
+                intent.putExtra("act_num", 0)
+                startActivity(intent)
+            }
         }
 
         back.setOnClickListener{
